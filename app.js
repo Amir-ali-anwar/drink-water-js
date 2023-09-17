652582fab10a2d703370d70cae4f0d315ae4e3b7
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-09-17 18:53:49 +0500

    working on the functinality

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const smallCups = document.querySelectorAll('.cup-small')
-const liters = document.getElementById('liters')
-const percentage = document.getElementById('percentage')
-const remained = document.getElementById('remained')
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+smallCups.forEach((cup, id) => {
+  console.log({ id });
+  cup.addEventListener("click", () => highLightedCup(id));
+});
 
-
-smallCups.forEach((cup,id)=>{
-    cup.addEventListener("click",()=>highLightedCup(id))
-})
-
-function highLightedCup(id){
-    console.log(id);
+function highLightedCup(id) {
+  if (
+    smallCups[id].classList.contains("full") &&
+    !smallCups[id].nextElementSibling.classList.contains("full")
+  ) {
+    id--;
+  }
+  smallCups.forEach((cup, index) => {
+    if (index <= id) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
 }
